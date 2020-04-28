a4e29c5bde708457b8754963ad53c80241e85880
Edwin <edwingenova@gmail.com>
2020-04-28 12:26:33 -0400

    fixed carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,11 +13,8 @@ export class Home extends React.Component {
                     <p>Click here to stake with us.</p>
                     <a className="btn btn-dark btn-lg" href="#" role="button">Learn more</a>
                 </div>
-                <h2>Stay up to date on Cardano with our news section.</h2>
-                <div id="carouselExampleCaptions" class="carousel slide container" data-ride="carousel"
-                style={{height: "100%",
-                        width: "100%"}}
-                >
+                <h2 className="container">Stay up to date on Cardano with our news section.</h2>
+                <div id="carouselExampleCaptions" class="carousel slide container" data-ride="carousel" style={{textAlign: "center"}}>
                     <ol class="carousel-indicators">
                         <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
                         <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
@@ -25,21 +22,21 @@ export class Home extends React.Component {
                     </ol>
                     <div class="carousel-inner container">
                         <div class="carousel-item active">
-                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed?v=OPtPipPN8qkuseQo"></iframe>
+                            <iframe width="716px" height="403px" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>First slide label</h5>
                                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                             </div>
                         </div>
                         <div class="carousel-item">
-                        <iframe src="https://www.youtube.com/embed?v=bhG_9Wk7ZFc"></iframe>
+                        <iframe width="716px" height="403px" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>Second slide label</h5>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                             </div>
                         </div>
                         <div class="carousel-item">
-                        <iframe src="https://www.youtube.com/embed?v=bhG_9Wk7ZFc"></iframe>
+                        <iframe width="716px" height="403px" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>Third slide label</h5>
                                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
